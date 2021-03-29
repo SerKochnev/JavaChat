d4f9ae9a8780aea20c9c5281ff6f0581a87b2197
RovI32 <sergey.kochnev7@gmail.com>
2021-03-29 19:42:13 +0300

    add some classes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+
 public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    @FXML
+    void sendMsg() {
+        if (textField.getText().equals("")) return;
+
+        textArea.appendText(" " + textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
 }
